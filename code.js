a2c0e012cf943d7546b7a994f9e8a2c8e5591243
lucasjanta <lucastyle2014@gmail.com>
2023-05-18 16:51:07 -0300

    First part of javascript

--- code.js
+++ code.js
@@ -1,2 +1,19 @@
 const mainslider = document.querySelector(".contentslider1");
 const api_url = "https://api.jikan.moe/v4/top/anime?limit=5";
+
+function animesmainslider() {
+    fetch(api_url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            console.log(data.data);
+            console.log(data.data[0]);
+            for (let i = 0; i < data.data.length; i++) {
+                const images = document.createElement('img');
+                images.setAttribute("src", data.data[i].images.webp.large_image_url);
+                mainslider.appendChild(images);
+            }
+        })
+        }
+
+animesmainslider();
